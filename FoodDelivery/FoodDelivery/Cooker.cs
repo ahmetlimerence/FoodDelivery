d9beb7d8f9fff91536a88248706955f675e0c12c
ahmetlimerence <95550306+ahmetlimerence@users.noreply.github.com>
2023-08-13 13:39:50 +0300

    Product and Food Classes created

---
+++ FoodDelivery/FoodDelivery/Cooker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery
+{
+    internal class Cooker
+    {
+        public string Adress {get; private set;}
+    }
+}
